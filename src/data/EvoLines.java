0f0b33a39c17ba719b24436c96f93a12e3e28d51
xCrystal <rgr.crystal@gmail.com>
2016-11-09 22:45:04 +0100

    Pokemon and evolutions data

---
+++ src/data/EvoLines.java
@@ -0,0 +1,187 @@
+package data;
+
+public enum EvoLines {
+
+	_BULBASAUR  (Pokemon.BULBASAUR,  Pokemon.IVYSAUR,    Pokemon.VENUSAUR),
+	_CHARMANDER (Pokemon.CHARMANDER, Pokemon.CHARMELEON, Pokemon.CHARIZARD),
+	_SQUIRTLE   (Pokemon.SQUIRTLE,   Pokemon.WARTORTLE,  Pokemon.BLASTOISE),
+	_CATERPIE   (Pokemon.CATERPIE,   Pokemon.METAPOD,    Pokemon.BUTTERFREE),
+	_WEEDLE     (Pokemon.WEEDLE,     Pokemon.KAKUNA,     Pokemon.BEEDRILL),
+	_PIDGEY     (Pokemon.PIDGEY,     Pokemon.PIDGEOTTO,  Pokemon.PIDGEOT),
+	_RATTATA    (Pokemon.RATTATA,    Pokemon.RATICATE),
+	_SPEAROW    (Pokemon.SPEAROW,    Pokemon.FEAROW),
+	_EKANS      (Pokemon.EKANS,      Pokemon.ARBOK),
+	_SANDSHREW  (Pokemon.SANDSHREW,  Pokemon.SANDSLASH),
+	_NIDORAN_F  (Pokemon.NIDORAN_F,  Pokemon.NIDORINA,   Pokemon.NIDOQUEEN),
+	_NIDORAN_M  (Pokemon.NIDORAN_M,  Pokemon.NIDORINO,   Pokemon.NIDOKING),
+	_VULPIX     (Pokemon.VULPIX,     Pokemon.NINETALES),
+	_ODDISH     (Pokemon.ODDISH,     Pokemon.GLOOM,      Pokemon.VILEPLUME),
+	_PARAS      (Pokemon.PARAS,      Pokemon.PARASECT),
+	_VENONAT    (Pokemon.VENONAT,    Pokemon.VENOMOTH),
+	_DIGLETT    (Pokemon.DIGLETT,    Pokemon.DUGTRIO),
+	_MEOWTH     (Pokemon.MEOWTH,     Pokemon.PERSIAN),
+	_PSYDUCK    (Pokemon.PSYDUCK,    Pokemon.GOLDUCK),
+	_MANKEY     (Pokemon.MANKEY,     Pokemon.PRIMEAPE),
+	_GROWLITHE  (Pokemon.GROWLITHE,  Pokemon.ARCANINE),
+	_POLIWAG    (Pokemon.POLIWAG,    Pokemon.POLIWHIRL,  Pokemon.POLIWRATH),
+	_ABRA       (Pokemon.ABRA,       Pokemon.KADABRA,    Pokemon.ALAKAZAM),
+	_MACHOP     (Pokemon.MACHOP,     Pokemon.MACHOKE,    Pokemon.MACHAMP),
+	_BELLSPROUT (Pokemon.BELLSPROUT, Pokemon.WEEPINBELL, Pokemon.VICTREEBEL),
+	_TENTACOOL  (Pokemon.TENTACOOL,  Pokemon.TENTACRUEL),
+	_GEODUDE    (Pokemon.GEODUDE,    Pokemon.GRAVELER,   Pokemon.GOLEM),
+	_PONYTA     (Pokemon.PONYTA,     Pokemon.RAPIDASH),
+	_SLOWPOKE   (Pokemon.SLOWPOKE,   Pokemon.SLOWBRO),
+	_MAGNEMITE  (Pokemon.MAGNEMITE,  Pokemon.MAGNETON),
+	_FARFETCH_D (Pokemon.FARFETCH_D),
+	_DODUO      (Pokemon.DODUO,      Pokemon.DODRIO),
+	_SEEL       (Pokemon.SEEL,       Pokemon.DEWGONG),
+	_GRIMER     (Pokemon.GRIMER,     Pokemon.MUK),
+	_SHELLDER   (Pokemon.SHELLDER,   Pokemon.CLOYSTER),
+	_GASTLY     (Pokemon.GASTLY,     Pokemon.HAUNTER,    Pokemon.GENGAR),
+	_ONIX       (Pokemon.ONIX,       Pokemon.STEELIX),
+	_DROWZEE    (Pokemon.DROWZEE,    Pokemon.HYPNO),
+	_KRABBY     (Pokemon.KRABBY,     Pokemon.KINGLER),
+	_VOLTORB    (Pokemon.VOLTORB,    Pokemon.ELECTRODE),
+	_EXEGGCUTE  (Pokemon.EXEGGCUTE,  Pokemon.EXEGGUTOR),
+	_CUBONE     (Pokemon.CUBONE,     Pokemon.MAROWAK),
+	_HITMONLEE  (Pokemon.HITMONLEE),
+	_HITMONCHAN (Pokemon.HITMONCHAN),
+	_LICKITUNG  (Pokemon.LICKITUNG),
+	_KOFFING    (Pokemon.KOFFING,    Pokemon.WEEZING),
+	_RHYHORN    (Pokemon.RHYHORN,    Pokemon.RHYDON),
+	_CHANSEY    (Pokemon.CHANSEY,    Pokemon.BLISSEY),
+	_TANGELA    (Pokemon.TANGELA),
+	_KANGASKHAN (Pokemon.KANGASKHAN),
+	_HORSEA     (Pokemon.HORSEA,     Pokemon.SEADRA,     Pokemon.KINGDRA),
+	_GOLDEEN    (Pokemon.GOLDEEN,    Pokemon.SEAKING),
+	_STARYU     (Pokemon.STARYU,     Pokemon.STARMIE),
+	_MR__MIME   (Pokemon.MR__MIME),
+	_SCYTHER    (Pokemon.SCYTHER,    Pokemon.SCIZOR),
+	_PINSIR     (Pokemon.PINSIR),
+	_TAUROS     (Pokemon.TAUROS),
+	_MAGIKARP   (Pokemon.MAGIKARP,   Pokemon.GYARADOS),
+	_LAPRAS     (Pokemon.LAPRAS),
+	_DITTO      (Pokemon.DITTO),
+	_EEVEE      (Pokemon.EEVEE,      Pokemon.VAPOREON),
+	_JOLTEON    (Pokemon.JOLTEON),
+	_FLAREON    (Pokemon.FLAREON),
+	_PORYGON    (Pokemon.PORYGON,    Pokemon.PORYGON2),
+	_OMANYTE    (Pokemon.OMANYTE,    Pokemon.OMASTAR),
+	_KABUTO     (Pokemon.KABUTO,     Pokemon.KABUTOPS),
+	_AERODACTYL (Pokemon.AERODACTYL),
+	_SNORLAX    (Pokemon.SNORLAX),
+	_ARTICUNO   (Pokemon.ARTICUNO),
+	_ZAPDOS     (Pokemon.ZAPDOS),
+	_MOLTRES    (Pokemon.MOLTRES),
+	_DRATINI    (Pokemon.DRATINI,    Pokemon.DRAGONAIR,  Pokemon.DRAGONITE),
+	_MEWTWO     (Pokemon.MEWTWO),
+	_MEW        (Pokemon.MEW),
+	_CHIKORITA  (Pokemon.CHIKORITA,  Pokemon.BAYLEEF,    Pokemon.MEGANIUM),
+	_CYNDAQUIL  (Pokemon.CYNDAQUIL,  Pokemon.QUILAVA,    Pokemon.TYPHLOSION),
+	_TOTODILE   (Pokemon.TOTODILE,   Pokemon.CROCONAW,   Pokemon.FERALIGATR),
+	_SENTRET    (Pokemon.SENTRET,    Pokemon.FURRET),
+	_HOOTHOOT   (Pokemon.HOOTHOOT,   Pokemon.NOCTOWL),
+	_LEDYBA     (Pokemon.LEDYBA,     Pokemon.LEDIAN),
+	_SPINARAK   (Pokemon.SPINARAK,   Pokemon.ARIADOS),
+	_ZUBAT      (Pokemon.ZUBAT,      Pokemon.GOLBAT,     Pokemon.CROBAT),
+	_CHINCHOU   (Pokemon.CHINCHOU,   Pokemon.LANTURN),
+	_PICHU      (Pokemon.PICHU,      Pokemon.PIKACHU,    Pokemon.RAICHU),
+	_CLEFFA     (Pokemon.CLEFFA,     Pokemon.CLEFAIRY,   Pokemon.CLEFABLE),
+	_IGGLYBUFF  (Pokemon.IGGLYBUFF,  Pokemon.JIGGLYPUFF, Pokemon.WIGGLYTUFF),
+	_TOGEPI     (Pokemon.TOGEPI,     Pokemon.TOGETIC),
+	_NATU       (Pokemon.NATU,       Pokemon.XATU),
+	_MAREEP     (Pokemon.MAREEP,     Pokemon.FLAAFFY,    Pokemon.AMPHAROS),
+	_BELLOSSOM  (Pokemon.BELLOSSOM),
+	_MARILL     (Pokemon.MARILL,     Pokemon.AZUMARILL),
+	_SUDOWOODO  (Pokemon.SUDOWOODO),
+	_POLITOED   (Pokemon.POLITOED),
+	_HOPPIP     (Pokemon.HOPPIP,     Pokemon.SKIPLOOM,   Pokemon.JUMPLUFF),
+	_AIPOM      (Pokemon.AIPOM),
+	_SUNKERN    (Pokemon.SUNKERN,    Pokemon.SUNFLORA),
+	_YANMA      (Pokemon.YANMA),
+	_WOOPER     (Pokemon.WOOPER,     Pokemon.QUAGSIRE),
+	_ESPEON     (Pokemon.ESPEON),
+	_UMBREON    (Pokemon.UMBREON),
+	_MURKROW    (Pokemon.MURKROW),
+	_SLOWKING   (Pokemon.SLOWKING),
+	_MISDREAVUS (Pokemon.MISDREAVUS),
+	_UNOWN      (Pokemon.UNOWN),
+	_WOBBUFFET  (Pokemon.WOBBUFFET),
+	_GIRAFARIG  (Pokemon.GIRAFARIG),
+	_PINECO     (Pokemon.PINECO),
+	_DUNSPARCE  (Pokemon.DUNSPARCE),
+	_GLIGAR     (Pokemon.GLIGAR),
+	_SNUBBULL   (Pokemon.SNUBBULL,   Pokemon.GRANBULL),
+	_QWILFISH   (Pokemon.QWILFISH),
+	_SHUCKLE    (Pokemon.SHUCKLE),
+	_HERACROSS  (Pokemon.HERACROSS),
+	_SNEASEL    (Pokemon.SNEASEL),
+	_TEDDIURSA  (Pokemon.TEDDIURSA,  Pokemon.URSARING),
+	_SLUGMA     (Pokemon.SLUGMA,     Pokemon.MAGCARGO),
+	_SWINUB     (Pokemon.SWINUB,     Pokemon.PILOSWINE),
+	_CORSOLA    (Pokemon.CORSOLA),
+	_REMORAID   (Pokemon.REMORAID,   Pokemon.OCTILLERY),
+	_DELIBIRD   (Pokemon.DELIBIRD),
+	_MANTINE    (Pokemon.MANTINE),
+	_SKARMORY   (Pokemon.SKARMORY),
+	_HOUNDOUR   (Pokemon.HOUNDOUR,   Pokemon.HOUNDOOM),
+	_PHANPY     (Pokemon.PHANPY,     Pokemon.DONPHAN),
+	_STANTLER   (Pokemon.STANTLER),
+	_SMEARGLE   (Pokemon.SMEARGLE),
+	_TYROGUE    (Pokemon.TYROGUE,    Pokemon.HITMONTOP),
+	_SMOOCHUM   (Pokemon.SMOOCHUM,   Pokemon.JYNX),
+	_ELEKID     (Pokemon.ELEKID,     Pokemon.ELECTABUZZ),
+	_MAGBY      (Pokemon.MAGBY,      Pokemon.MAGMAR),
+	_MILTANK    (Pokemon.MILTANK),
+	_RAIKOU     (Pokemon.RAIKOU),
+	_ENTEI      (Pokemon.ENTEI),
+	_SUICUNE    (Pokemon.SUICUNE),
+	_LARVITAR   (Pokemon.LARVITAR,   Pokemon.PUPITAR,    Pokemon.TYRANITAR),
+	_LUGIA      (Pokemon.LUGIA),
+	_HO_OH      (Pokemon.HO_OH),
+	_CELEBI     (Pokemon.CELEBI);
+	
+	private Pokemon pkmn1;
+	private Pokemon pkmn2;
+	private Pokemon pkmn3;
+	
+	EvoLines (Pokemon pkmn1) {
+		this.setPkmn1(pkmn1);
+	}	
+	
+	EvoLines (Pokemon pkmn1, Pokemon pkmn2) {
+		this.setPkmn1(pkmn1);
+		this.setPkmn2(pkmn2);
+	}	
+	
+	EvoLines (Pokemon pkmn1, Pokemon pkmn2, Pokemon pkmn3) {
+		this.setPkmn1(pkmn1);
+		this.setPkmn2(pkmn2);
+		this.setPkmn3(pkmn3);
+	}
+
+	public Pokemon getPkmn1() {
+		return pkmn1;
+	}
+
+	public void setPkmn1(Pokemon pkmn1) {
+		this.pkmn1 = pkmn1;
+	}
+
+	public Pokemon getPkmn2() {
+		return pkmn2;
+	}
+
+	public void setPkmn2(Pokemon pkmn2) {
+		this.pkmn2 = pkmn2;
+	}
+
+	public Pokemon getPkmn3() {
+		return pkmn3;
+	}
+
+	public void setPkmn3(Pokemon pkmn3) {
+		this.pkmn3 = pkmn3;
+	}
+	
+}
---
+++ src/data/EvoTypes.java
@@ -0,0 +1,12 @@
+package data;
+
+public enum EvoTypes {
+	
+	EVOTYPE1OF1,
+	EVOTYPE1OF2,
+	EVOTYPE1OF3,
+	EVOTYPE2OF2,
+	EVOTYPE2OF3,
+	EVOTYPE3OF3,
+	LEGEND;
+}
